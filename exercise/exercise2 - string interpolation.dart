3e43f39ade7fe808dfed9576dff21724e754052a
Donlawat Samerwongtip <65522118+Daitsuku@users.noreply.github.com>
2023-07-08 10:58:54 +0700

    second commit
    changes file name

---
+++ exercise/exercise2 - string interpolation.dart
@@ -0,0 +1,10 @@
+void main() {
+  double tempurature = 20;
+  int value = 2;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+
+  print("The tempurature is ${tempurature}C");
+  print("$value plus $value makes ${value + value}");
+  print("I like $pizza and $pasta");
+}
